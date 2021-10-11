a0dd065cebd68e79e5a10db84ba5ee681d99daed
Mahdi Samiee <mahdisamieefarisar@outlook.com>
2021-10-11 14:23:25 +0330

    fixed some bugs

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const filterOption = document.querySelector(".filter-todo");
 // ~? Selectors
 
 // ! event listener
-getTodos();
 todoBtn.addEventListener("click", addTodo);
 todoList.addEventListener("click", deletecheck);
 filterOption.addEventListener("click", filterTodo);
+document.addEventListener("DOMContentLoaded", fetchItemFromStorage);
 //~! event listener
 
-// @ functions
+// # functions
 function addTodo(event) {
   event.preventDefault();
   const newListDivision = document.createElement("div");
@@ -26,14 +26,13 @@ function addTodo(event) {
     localStorageAddItem(todoInput.value);
   } else {
     newListItem.innerText = `Empty Input ${todoList.children.length + 1}`;
-    localStorageAddItem(`Empty Input ${todoList.children.length + 1}`);
   }
   todoInput.value = "";
   newListDivision.appendChild(newListItem);
 
   // // create checkbox
   itemCheckbox.classList.add("list-check-btn");
-  itemCheckbox.innerHTML = '<i class="fas fa-check"></i>';
+  itemCheckbox.innerHTML = '<i class="fas fa-square enactive"></i>';
   newListDivision.appendChild(itemCheckbox);
 
   // ? create close button
@@ -45,15 +44,27 @@ function addTodo(event) {
 }
 function deletecheck(event) {
   const item = event.target;
+  console.log(item);
   const parentDiv = item.parentElement;
-  if (item.classList[0] === "list-del-btn") {
+  switch (item.classList[0]) {
+    case "list-del-btn":
       parentDiv.classList.add("fall");
-      localStorageRemoveItem(parentDiv.children[0]);
       parentDiv.addEventListener("transitionend", function () {
+        localStorageRemoveItem(parentDiv);
         parentDiv.remove();
-    });
-  } else if (item.classList[0] === "list-check-btn") {
-    parentDiv.classList.toggle("done");
+      });
+      break;
+    case "list-check-btn":
+      if (item.children[0].classList[2] === "enactive") {
+        parentDiv.classList.toggle("done");
+        item.children[0].remove();
+        item.innerHTML = `<i class="fas fa-check-square active"></i>`;
+      } else if (item.children[0].classList[2] === "active") {
+        parentDiv.classList.toggle("done");
+        item.children[0].remove();
+        item.innerHTML = `<i class="fas fa-square enactive"></i>`;
+      }
+      break;
   }
 }
 function filterTodo(event) {
@@ -91,42 +102,41 @@ function localStorageCheck() {
 }
 function localStorageAddItem(value) {
   let todos = localStorageCheck();
-  todos.push(value);
+  todos.push(value.toLowerCase());
   localStorage.setItem("localTodos", JSON.stringify(todos));
 }
-function localStorageRemoveItem(value) {
-  let todos = localStorageCheck();
-  let txt = value.innerText.toLowerCase();
-  todos.splice(todos.indexOf(txt), 1);
+function localStorageRemoveItem(item) {
+  const todos = localStorageCheck();
+  const itemValue = item.children[0].innerText.toLowerCase();
+  const index = todos.indexOf(itemValue);
+  todos.splice(index, 1);
   if (todos.length === 0) {
     localStorage.removeItem("localTodos");
   } else {
     localStorage.setItem("localTodos", JSON.stringify(todos));
   }
 }
-function getTodos() {
+function fetchItemFromStorage() {
   let todos = localStorageCheck();
-  todos.forEach(function (todo) {
+  for (const todo of todos) {
     const newListDivision = document.createElement("div");
-    newListDivision.classList.add("todo-div");
     const newListItem = document.createElement("li");
     const itemDelBtn = document.createElement("button");
     const itemCheckbox = document.createElement("button");
+
+    newListDivision.classList.add("todo-div");
     newListItem.classList.add("list-item");
-    newListItem.innerText = todo;
-    newListDivision.appendChild(newListItem);
-
-    // // create checkbox
     itemCheckbox.classList.add("list-check-btn");
-    itemCheckbox.innerHTML = '<i class="fas fa-check"></i>';
-    newListDivision.appendChild(itemCheckbox);
-
-    // ? create close button
     itemDelBtn.classList.add("list-del-btn");
-    itemDelBtn.innerHTML = '<i class="fas fa-trash"></i>';
-    newListDivision.appendChild(itemDelBtn);
 
+    newListItem.innerText = todo;
+    itemCheckbox.innerHTML = '<i class="fas fa-square enactive"></i>';
+    itemDelBtn.innerHTML = '<i class="fas fa-trash"></i>';
+
+    newListDivision.appendChild(newListItem);
+    newListDivision.appendChild(itemCheckbox);
+    newListDivision.appendChild(itemDelBtn);
     todoList.appendChild(newListDivision);
-  });
+  }
 }
-// ~@ functions
+// ~# functions
